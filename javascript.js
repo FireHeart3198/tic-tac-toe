855206c8caff4129be472b08cd6bd512c90a910b
mb <115391907+FireHeart3198@users.noreply.github.com>
2024-03-16 21:07:31 +1100

    Add player factory function

--- javascript.js
+++ javascript.js
@@ -1,18 +1,33 @@
 const gameBoard = (function() {
-    const gameSquares = []
+    const gameSquares = [];
     
     // create game squares at game start so don't have to reset to null at end
     const createGameSquares = function() {
-        gameSquares.splice(0,gameSquares.length)
+        gameSquares.splice(0,gameSquares.length);
         for (let i = 1; i <= 9; i++) {
-            let square = {squareNum: i, status: null,}
-            gameSquares.push(square)
+            let square = {squareNum: i, status: null,};
+            gameSquares.push(square);
         }
-        console.log(gameSquares)
     }
     const updateSquare = function (squareNum, updatedStatus) {
-        gameSquares[squareNum - 1].status = updatedStatus;}
-    const getSquareStatus = (squareNum) => gameSquares[squareNum - 1].status
+        gameSquares[squareNum - 1].status = updatedStatus;
+    }
+    const getSquareStatus = (squareNum) => gameSquares[squareNum - 1].status;
 
-    return { updateSquare, getSquareStatus, createGameSquares}
+    return { updateSquare, getSquareStatus, createGameSquares };
 })();
+
+
+function player(playerName, playerMark) {
+    const playerInfo = {
+        playerName,
+        playerMark,
+    };
+
+    const getPlayerInfo = () => playerInfo;
+    return { getPlayerInfo };
+}
+
+josh = player("josh", "x")
+console.log(josh)
+console.log(josh.getPlayerInfo())
